ab3416d2d3721561827e3ae54df9f0df21ba8fdd
saptaparna <saptaparna@gmail.com>
2021-03-22 11:18:40 -0500

    reading a generic rootfile

--- readStopLHE.py
+++ readStopLHE.py
@@ -55,23 +55,10 @@ if __name__ == '__main__':
             if (abs(p['ID'])  == 24 and rt.TMath.Sqrt(p['Px']*p['Px'] + p['Py']*p['Py']) > 50.0): pTW_jj.Fill(rt.TMath.Sqrt(p['Px']*p['Px'] + p['Py']*p['Py']))
             if abs(p['ID']) == 13: 
               n_mu += 1
-              if n_mu==1:  mu1_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
-              if n_mu==2:  mu2_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
-              if n_mu==2:  MInvariantMass_mumu.Fill((mu1_lv+mu2_lv).M())
             if ((abs(p['ID']) == 1 or abs(p['ID']) == 2 or abs(p['ID']) == 3 or abs(p['ID']) == 4)):
               n_q += 1
               print n_q
               #print p['Px'], p['Py'], p['Pz'], p['E']
-              if n_q==3: q1_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
-              if n_q==4: q2_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
-              if n_q==4: MInvariantMass_qq.Fill((q1_lv+q2_lv).M())
-              if n_q==4: EW_qq.Fill((q1_lv+q2_lv).E());
-              if n_q==4: DeltaR_qq.Fill(q1_lv.DeltaR(q2_lv)); #q1_lv.Pt() > 180.0 and q2_lv.Pt() > 180.0): DeltaR_qq.Fill(q1_lv.DeltaR(q2_lv));
-              if n_q==4: DeltaR_qq_pT.Fill(q1_lv.DeltaR(q2_lv), (q1_lv.Pt()+q2_lv.Pt())/2.0)
-              if n_q==4 and ((q1_lv+q2_lv).M() > 70.0 and (q1_lv+q2_lv).M() < 90.0): DeltaR_qq_pT_Wmass.Fill(q1_lv.DeltaR(q2_lv), (q1_lv.Pt()+q2_lv.Pt())/2.0) 
-              if n_q==4: h_boostedpT_deltaR04.Fill((q1_lv.Pt()+q2_lv.Pt()))
-              #if n_q==4 and q1_lv.DeltaR(q2_lv) < 0.4: h_boostedpT_deltaR04.Fill((q1_lv.Pt()+q2_lv.Pt())/2.0)
-              if n_q==4 and (q1_lv.Pt()+q2_lv.Pt())/2.0 > 150.0: h_boostedDeltaR_pT150.Fill(q1_lv.DeltaR(q2_lv)) 
               if (q1_lv+q2_lv).M() > 0.0: gamma_qq.Fill((q1_lv+q2_lv).E()/(q1_lv+q2_lv).M());
         del oneEvent, myLHEevent
         
